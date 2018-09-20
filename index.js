e26529f0345e33bf2f16e61deda5bea70b3dc869
Ezra T Huffman <joshooha@gmail.com>
2018-09-20 11:40:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var newArr = {};
+  var myString = "";
+  for (i = 0; i < musicians.length; i++) {
+    myString = musicians[i] + instruments[i];
+    newArr.push(myString);
+  }
+  return newArr;
+}
+
+function johnLennonFacts(facts) {
+  var counter = 0;
+  while (index < facts.length) {
+    facts[index] = facts[index] + "!!!";
+    counter++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(n) {
+  var newArr = {};
+  do {
+    newArr.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return newArr;
+}
